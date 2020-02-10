de11f8ab1bdc1d617489159dee868bcf67a1e85c
dokturpjoter <59878470+dokturpjoter@users.noreply.github.com>
2020-02-10 01:12:21 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -34,8 +34,6 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
